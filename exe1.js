8c7cc2af8191a584394ad3927edc1142277f5070
Oamerdou <omarfyo19@gmail.com>
2026-04-23 19:07:15 +0200

    exercice1

---
+++ exe1.js
@@ -0,0 +1,13 @@
+let firstInput = prompt("Enter the first number:");
+let secondInput = prompt("Enter the second number:");
+let num1 = parseFloat(firstInput);
+let num2 = parseFloat(secondInput);
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+console.log("Results:");
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
